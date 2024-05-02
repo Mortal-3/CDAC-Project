10b164e4d037fdc2e22cc11f3fcd2db7529ff524
Mortal <oo7vishalkumar03@gmail.com>
2024-05-02 16:22:14 +0530

    contact page db created and connected with database sucessfully

---
+++ simulator/home/views.py
@@ -0,0 +1,50 @@
+
+from django.shortcuts import render,HttpResponse,redirect
+from django.http import FileResponse
+import os
+from home.models import Contact
+# from .models import Contact
+from datetime import datetime
+from django.contrib import messages
+
+# messages.add_message(request, messages.INFO, "Hello world.")
+
+from django.utils import timezone
+
+
+
+# Create your views here.
+def index(request):
+    # messages.sucess(request)
+    numbers=list(range(20))
+    # return render(request,'simulator.html',context)
+    return render(request,'simulator.html',{'numbers':numbers})
+def simulator(request):
+    numbers=list(range(20))
+    # return render(request,'simulator.html',context)
+    return render(request,'simulator.html',{'numbers':numbers})
+def about(request):
+ 
+    return render(request,'about.html')
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+       
+        # Create an instance of the Contact model and save it to the database
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
+        contact.save()
+        messages.success(request, "Your details has been sucessfully send...!")  # ignored
+       
+
+        return render(request, 'contact.html')
+    else:
+        return render(request, 'contact.html')  # Render contact.html for GET requests
+
+def quantum_grocery(request):
+ 
+        return render("Data uploaded Soon ! :)")
+
