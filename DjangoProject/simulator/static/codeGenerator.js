7dbc12315d0c0b18dce2e52c17a85a8dc5893280
Mortal <oo7vishalkumar03@gmail.com>
2024-05-28 11:00:09 +0530

    new Change implimentation begin

---
+++ DjangoProject/simulator/static/codeGenerator.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var permissionSelect = document.getElementById("permission");
+  var myCodeDiv = document.getElementById("myCode");
+
+  console.log("myDiv Code Section : ", myCodeDiv);
+  console.log("myDiv Code Section control : ", permissionSelect);
+
+  permissionSelect.addEventListener("change", function () {
+    if (this.value === "write") {
+      myCodeDiv.contentEditable = "true";
+      myCodeDiv.style.display = "block"; // Show the div
+      myCodeDiv.style.backgroundColor = "#f8f9fa"; // Light background color for editing
+      myCodeDiv.style.border = "1px solid #ced4da"; // Add a border to indicate editability
+      myCodeDiv.style.padding = "10px"; // Padding for better text visibility
+    } else {
+      myCodeDiv.contentEditable = "false";
+      myCodeDiv.style.display = "none"; // Hide the div
+    }
+    console.log("SET Data is true ", myCodeDiv);
+  });
+});
