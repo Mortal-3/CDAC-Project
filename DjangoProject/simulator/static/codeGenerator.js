20a5e4914f755938311426f636e13cfddbd4caf0
Mortal <oo7vishalkumar03@gmail.com>
2024-06-07 09:46:37 +0530

    Removed extal tine of code and reduced it

--- DjangoProject/simulator/static/codeGenerator.js
+++ DjangoProject/simulator/static/codeGenerator.js
@@ -6,9 +6,10 @@ document.addEventListener("DOMContentLoaded", function () {
     if (this.value === "write") {
       myCodeDiv.contentEditable = "true";
       myCodeDiv.style.display = "block"; // Show the div
-      myCodeDiv.style.backgroundColor = "#f8f9fa"; // Light background color for editing
-      myCodeDiv.style.border = "1px solid #ced4da"; // Add a border to indicate editability
-      myCodeDiv.style.padding = "10px"; // Padding for better text visibility
+      myCodeDiv.style.backgroundColor = "#f8f9fa";
+      myCodeDiv.style.border = "1px solid #ced4da";
+      editability;
+      myCodeDiv.style.padding = "10px";
     } else {
       myCodeDiv.contentEditable = "false";
       myCodeDiv.style.display = "none"; // Hide the div
